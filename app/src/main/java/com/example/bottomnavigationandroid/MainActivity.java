b17b30a72484a8ff5fdb5bf0779a31363a329041
mits <devanshu18g@gmail.com>
2021-05-09 14:02:02 +0530

    Project Tested & Done

--- app/src/main/java/com/example/bottomnavigationandroid/MainActivity.java
+++ app/src/main/java/com/example/bottomnavigationandroid/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.bottomnavigationandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textview = findViewById(R.id.textView);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+        (item) -> {
+            switch (item.getItemId()) {
+                case R.id.update_item:
+                    textview.setText("Update Item Selected");
+                    break;
+                case R.id.location_item:
+                    textview.setText("location Item Selected");
+                    break;
+                case R.id.favorite_item:
+                    textview.setText("favorite Item Selected");
+                    break;
+
+            }
+        return true;
+        });
     }
 }
